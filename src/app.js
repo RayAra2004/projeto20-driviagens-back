12875f6bd2507a2bd392b25ac436d6a9523d492f
RayAra2004 <raynan.araujo@gmail.com>
2023-09-05 18:19:12 -0300

    Setup initial

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
